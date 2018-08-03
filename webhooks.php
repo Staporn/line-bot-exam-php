82733c3b84afd0a73ded7f5a99abe64cf12f67f2
T'Staporn <31606471+Staporn@users.noreply.github.com>
2018-08-03 18:00:05 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '0ewzxOq6unJ7IjxXHIALEe8aCs7MFUs0OLauA3A0qoAdE0XneACYbZ/22bgkp9wQWjPvyY027EKdq3BADzn9Ncn4ArMSo+fHZcAkzDHpy6AHBKKpkb8RCG3tqdfLfmSdoERfYW+iuy5lkweNm2iZOwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
